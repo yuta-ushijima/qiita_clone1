39ee608607f03c2590161bfc34eecc033ccc830b
Takahiro Abe <hirotaka081630@gmail.com>
2019-09-12 05:38:57 +0900

    application.rbの設定変更

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,16 @@ module QiitaClone1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
     config.generators do |g|
-      # Settings in config/environments/* take precedence over those specified here.
-      # Application configuration can go into files in config/initializers
-      # -- all .rb files in that directory are automatically loaded after loading
-      # the framework and any gems in your application.
-      g.test_framework :rspec
+      g.javascripts false
+      g.styesheets false
+      g.helper false
+      g.test_framework false
     end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
