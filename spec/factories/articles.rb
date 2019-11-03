899abf7cf21930244ba8ed432f5d4b652349fa2e
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-11-03 19:31:30 +0900

    ArticleModelのテスト作成完了

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyText" }
+    title { Faker::Lorem.characters(number: Random.new.rand(1..64)) }
+    body { Faker::Lorem.characters }
     user_id { 1 }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "正常系" do
+    let(:article) { build(:article) }
+
+    it "記事の作成・保存完了" do
+      expect(article).to be_valid
+    end
+  end
+
+  context "titleがないとき" do
+    let(:article) { build(:article, title: nil) }
+
+    it "エラーする" do
+      article.valid?
+      expect(article.errors.messages[:title]).to include "can't be blank"
+    end
+  end
+
+  context "titleが長すぎるとき" do
+    let(:article) { build(:article, title: Faker::Lorem.characters(number: Random.new.rand(65..999)))}
+
+    it "エラーする" do
+      article.valid?
+      expect(article.errors.messages[:title]).to include "is too long (maximum is 64 characters)"
+    end
+  end
 end
