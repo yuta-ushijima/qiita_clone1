150c412d12a9e5b0abf41d9e42aef97c5746f236
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-11-04 10:01:53 +0900

    api_controllerの作成

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module QiitaClone1
     config.load_defaults 5.2
 
     config.generators do |g|
+      g.template_engine false
       g.javascripts false
       g.styesheets false
       g.helper false
@@ -23,6 +24,8 @@ module QiitaClone1
                        controller_specs: false,
                        request_specs: true
     end
+
+    config.api_only = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :articles
+    end
+  end
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    end
