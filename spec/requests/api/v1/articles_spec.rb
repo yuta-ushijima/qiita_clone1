ada33b83ac5b6566d1bcb538050561e96800c67a
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-11-10 13:39:15 +0900

    記事詳細の実装　routeの確認と修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
   end
 
   describe "GET /api/v1/articles/:id" do
-    subject { get(api_v1_articles_path(article_id)) }
+    subject { get(api_v1_article_path(article_id)) }
 
     context "指定したidの記事が存在する場合" do
       let(:article){ create(:article) }
@@ -32,9 +32,9 @@ RSpec.describe "Api::V1::Articles", type: :request do
         res = JSON.parse(response.body)
 
         expect(response).to have_http_status(200)
-        expect(res[0]["id"]).to eq article.id
-        expect(res[0]["title"]).to eq article.title
-        expect(res[0]["body"]).to eq article.body
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
       end
     end
 
@@ -42,7 +42,6 @@ RSpec.describe "Api::V1::Articles", type: :request do
       let(:article_id){ 100000 }
 
       it "記事が見つからない" do
-        subject
         expect { subject }.to raise_error ActiveRecord::RecordNotFound
       end
     end
