5f41dfee99a29e2024012b190d78e1e89f6fc81c
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-11-10 10:34:35 +0900

    api_controllerの名前の変更

--- app/controllers/api/v1/api_controller.rb
+++
@@ -1,3 +0,0 @@
-class Api::V1::ApiController < ApplicationController
-  include DeviseTokenAuth::Concerns::SetUserByToken
-end
--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ArticlesController < Api::V1::ApiController
+class Api::V1::ArticlesController < Api::V1::BaseApiController
   def index
     @articles = Article.all
     render json: @articles
