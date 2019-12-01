4c160f89a2a18c89e852c764db96516a3e6b6239
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-12-01 18:31:43 +0900

    トップページの作成まで

---
+++ app/javascript/packs/app.ts
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "./router/router";
+import Header from "./container/Header.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "highlight.js/styles/monokai.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: "fa"
+});
+
+document.addEventListener("turbolinks:load", () => {
+  new Vue({
+    el: "#app",
+    router: Router,
+    components: {
+      navbar: Header
+    }
+  });
+});
