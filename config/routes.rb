65bb211c5d5c6097972233d7ab99804ef3d60c7f
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-12-01 20:05:17 +0900

    fix rubocop settings and autocorrect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'homes#index'
+  root "homes#index"
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
