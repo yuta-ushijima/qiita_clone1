441caf060d3787d13ec0c5fd77a181f6cf0de2c3
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-12-04 04:49:11 +0900

    set sign_up_params in registrations_controller

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,1 +1,7 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+private
+  def sign_up_params
+    #新規登録時のパラメータの制限の定義をしている
+    params.permit(:name, :email, :password )
+  end
+end
