cb1ff46cea07f345765f0b4286ce3fce2cda5b01
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-12-08 20:41:23 +0900

    destroyメソッドの実装準備

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
   #before_action :authenticate_api_v1_user!
   #基本的にメソッドは継承元のものを使えば良いのでここでではパラメータの制限のみを記述している。
+  skip_before_action :verify_authenticity_token
+  def create
+    super
+  end
+
+  def destroy
+    #@resource = params
+    binding.pry
+    super
+  end
+
   private
 
   def session_params
     params.require(:session).permit(:email, :password)
   end
+
 end
