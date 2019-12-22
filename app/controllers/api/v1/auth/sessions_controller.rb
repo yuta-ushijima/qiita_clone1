9218b3214204a38d9a1f4b0887a07abd83ea6aa5
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2019-12-22 18:44:44 +0900

    コメントの削除

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
   before_action :authenticate_user!, only: [:destroy]
-  #基本的にメソッドは継承元のものを使えば良いのでここでではパラメータの制限のみを記述している。
 
   def create
     super
