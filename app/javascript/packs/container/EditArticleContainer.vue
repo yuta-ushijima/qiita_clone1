26b33814f10a9d5eb78484c32ce7eaffb40f22ee
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-06 03:32:16 +0900

    新規記事投稿ページの作成

---
+++ app/javascript/packs/container/EditArticleContainer.vue
@@ -0,0 +1,68 @@
+<template>
+  <form class="article-form">
+    <v-text-field outline single-line v-model="title" name="title" label="タイトル" class="title-form"></v-text-field>
+    <v-textarea
+      outline
+      no-resize
+      height="100%"
+      v-model="body"
+      name="body"
+      label="プログラミング知識をMarkdown記法で書いて共有しよう"
+      class="body-form"
+    ></v-textarea>
+  </form>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import { Vue, Component } from "vue-property-decorator";
+
+const headers = {
+  headers: {
+    Authorization: "Bearer",
+    "Access-Control-Allow-Origin": "*",
+    "access-token": localStorage.getItem("access-token"),
+    client: localStorage.getItem("client"),
+    uid: localStorage.getItem("uid")
+  }
+};
+
+@Component
+export default class ArticlesContainer extends Vue {
+  articles: string[] = [];
+
+  async mounted(): Promise<void> {
+    await this.fetchArticles();
+  }
+
+  async fetchArticles(): Promise<void> {
+    await axios.get("/api/v1/articles").then(response => {
+      response.data.map((article: any) => {
+        this.articles.push(article);
+      });
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.article-form {
+  margin: 5px;
+  height: calc(100% - 64px - 10px);
+  display: flex;
+  flex-flow: column;
+}
+.title-form {
+  flex: none;
+}
+</style>
+
+<style lang="scss">
+.body-form > .v-input__control {
+  height: 100%;
+}
+
+.v-text-field .v-text-field__details {
+  display: none;
+}
+</style>
