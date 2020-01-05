3a9f8f5cdb1b2131eca1a8ae0d867f7af42cbc4c
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-06 04:07:51 +0900

    fix article detail design

--- app/javascript/packs/container/ArticleContainer.vue
+++ app/javascript/packs/container/ArticleContainer.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-container class="item elevation-3 articles-container"></v-container>
+  <v-container v-model="article" class="item elevation-3 article-container">
+    <v-layout xs-12 class="top-info-container">
+      <span class="user-name">@{{ article.user.name }}</span>
+      <time-ago :refresh="60" :datetime="article.updated_at" locale="en" tooltip="top" long></time-ago>
+    </v-layout>
+    <v-layout>
+      <h1 class="article-title">{{ article.title }}</h1>
+    </v-layout>
+    <v-layout class="article-body-container">
+      <div id="article-body">{{ article.body }}</div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -16,19 +27,39 @@ export default class ArticleContainer extends Vue {
   article: any = "";
 
   async mounted(): Promise<void> {
-    await this.fetchArticle("1");
+    await this.fetchArticle(this.$route.params.id);
   }
 
   async fetchArticle(id: string): Promise<void> {
-    await axios.get(`/api/v1/articles/${id}`).then(response => {
-      this.article = response.data;
-    });
+    await axios
+      .get(`/api/v1/articles/${id}`)
+      .then(response => {
+        this.article = response.data;
+      })
+      .catch(e => {
+        // TODO: 適切な Error 表示
+        alert(e.response.statusText);
+      });
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.articles-container {
-  margin-top: 2em;
+.top-info-container {
+  margin-bottom: 1em;
+}
+.article-container {
+  margin: 2em;
+}
+.article-title {
+  font-size: 2.5em;
+  line-height: 1.4;
+}
+.article-body-container {
+  margin: 2em 0;
+  font-size: 16px;
+}
+.user-name {
+  margin-right: 1em;
 }
 </style>
