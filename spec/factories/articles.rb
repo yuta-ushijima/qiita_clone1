233b1472d5db080a928334025869824b308d85a6
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-10 22:25:38 +0900

    Article Model のfactorybotにstatus columnを追加

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,9 +3,14 @@ FactoryBot.define do
     title { Faker::Lorem.characters(number: Random.new.rand(1..64)) }
     body { Faker::Lorem.characters }
     user
+    status { :draft }
 
     trait :with_comment do
       comment
+    end
+
+    trait :published do
+      status { :published }
     end
   end
 end
