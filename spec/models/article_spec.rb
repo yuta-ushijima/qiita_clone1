0e2fb0a01c26952a9d1c8edd38be900d04f79303
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-12 14:24:17 +0900

    enumによるメソッドの確認テストを実装

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Article, type: :model do
     end
   end
 
-  context "正常系（statusがpubrishedのとき）" do
+  context "正常系（statusがpublishedのとき）" do
     let(:article) { build(:article, status: "published") }
 
     it "公開記事として有効である" do
@@ -36,6 +36,104 @@ RSpec.describe Article, type: :model do
     it "エラーする" do
       article.valid?
       expect(article.errors.messages[:title]).to include "is too long (maximum is 64 characters)"
+    end
+  end
+end
+
+describe "#draft" do
+  before do
+    create_list(:article, 10)
+  end
+
+  it "draftの検索ができる" do
+    expect(Article.draft).to eq Article.where(status: "draft")
+  end
+end
+
+describe "#published" do
+  before do
+    create_list(:article, 10 ,:published)
+  end
+
+  it "publishedの検索ができる" do
+    expect(Article.published).to eq Article.where(status: "published")
+  end
+end
+
+
+
+describe "#.status" do
+  context "statusがdraftのとき" do
+    let(:article) { create(:article, :draft) }
+
+    it "draftが返ってくる" do
+      expect(article.status).to eq "draft"
+    end
+  end
+
+  context "statusがpublishedのとき" do
+    let(:article) { create(:article, :published) }
+
+    it "publishedが返ってくる" do
+      expect(article.status).to eq "published"
+    end
+  end
+end
+
+describe "#.draft?" do
+  context "statusがdraftのとき" do
+    let(:article) { create(:article, :draft) }
+
+    it "trueが返ってくる" do
+      expect(article.draft?).to eq true
+    end
+  end
+
+  context "statusがpublishedのとき" do
+    let(:article) { create(:article, :published) }
+
+    it "false" do
+      expect(article.draft?).to eq false
+    end
+  end
+end
+
+describe "#.published?" do
+  context "draftのとき" do
+    let(:article) { create(:article, :draft) }
+
+    it "false" do
+      expect(article.published?).to eq false
+    end
+  end
+
+  context "publishedのとき" do
+      let(:article) { create(:article, :published) }
+
+      it "true" do
+        expect(article.published?).to eq true
+      end
+  end
+end
+
+describe "#.published!" do
+  context "draftのとき" do
+    let(:article) { create(:article, :draft) }
+
+    it "publishedに更新される" do
+      expect(article.published!).to eq true
+      expect(article[:status]).to eq "published"
+    end
+  end
+end
+
+describe "#.draft!" do
+  context "publishedのとき" do
+    let(:article) { create(:article, :published) }
+
+    it "draftに更新する" do
+      expect(article.draft!).to eq true
+      expect(article[:status]).to eq "draft"
     end
   end
 end
