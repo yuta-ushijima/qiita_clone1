92dd7be8b920d53a36ed3200bb3ec6c4f8cbcefc
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-12 21:55:24 +0900

    response.bodyのカラムにstatusを追加

--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :updated_at
+  attributes :id, :title, :body, :updated_at, :status
 
   belongs_to :user
 end
