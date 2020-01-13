ca61506ec523c98215823d26b99b4f46c0e9b65c
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-13 22:12:08 +0900

    rubocopの実行

--- spec/requests/api/v1/articles/drafts_spec.rb
+++ spec/requests/api/v1/articles/drafts_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
     let(:headers) { authenticate_user_headers(current_user) }
     let(:other_user_id) { other_user.id }
 
-
     it "自分の下書き記事のみ一覧になる" do
       subject
       res = JSON.parse(response.body)
@@ -25,7 +24,7 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
       expect(res.length).to eq current_user.articles.draft.count
       expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "status", "user"]
 
-      #本当はメソッドとして定義したい
+      # 本当はメソッドとして定義したい
       i = 0
       ary_status = Array.new(len)
       count_user = 0
@@ -39,11 +38,10 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
         i += 1
       end
 
-      #下書き記事のみ取得している
+      # 下書き記事のみ取得している
       expect(ary_status.include?("published")).to eq false
-      #自分の記事のみ取得している
+      # 自分の記事のみ取得している
       expect(count_user).to eq 0
-
     end
   end
 
@@ -71,7 +69,7 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
 
     context "自分の公開記事を指定したとき" do
       let(:article) { create(:article, :published, user: current_user) }
-      let(:article_id) { article.id}
+      let(:article_id) { article.id }
 
       it "表示できない" do
         binding.pry
@@ -88,5 +86,4 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
       end
     end
   end
-
 end
