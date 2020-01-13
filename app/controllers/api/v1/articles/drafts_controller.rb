ad8911fb560df045706a591e670ef7d0154f8f21
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-13 15:09:20 +0900

    index,showメソッドの実装

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::Articles::DraftsController < Api::V1::BaseApiController
   before_action :authenticate_user!
 
   def index
-    drafts = current_user.Article.drafts
+    drafts = current_user.articles.draft
     render json: drafts
   end
 
   def show
-    draft = current_user.Article.draft.find(params[:id])
+    draft = current_user.articles.draft.find(params[:id])
     render json: draft
   end
 end
