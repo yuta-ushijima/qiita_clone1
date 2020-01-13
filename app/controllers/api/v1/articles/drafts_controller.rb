1058a38344cde047f2689b70fe5b60d7644e6a4b
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-13 15:07:57 +0900

    routing errorによるファイルの移動

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -1,4 +1,4 @@
-class Controllers::Api::V1::Articles::DraftsController < Api::V1::BaseApiController
+class Api::V1::Articles::DraftsController < Api::V1::BaseApiController
   before_action :authenticate_user!
 
   def index
