7fd5561932006580401e8910c2a13dcc5cbec2ef
Takahiro Abe <takahiro@TakahironoMacBook-puro.local>
2020-01-13 13:30:31 +0900

    drafts_controllerの作成

---
+++ app/controllers/controllers/api/v1/articles/drafts_controller.rb
@@ -0,0 +1,13 @@
+class Controllers::Api::V1::Articles::DraftsController < Api::V1::BaseApiController
+  before_action :authenticate_user!
+
+  def index
+    drafts = current_user.Article.drafts
+    render json: drafts
+  end
+
+  def show
+    draft = current_user.Article.draft.find(params[:id])
+    render json: draft
+  end
+end
