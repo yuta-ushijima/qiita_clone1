87d1d2f35a736ca01f779207bda13dbfe6e19a8d
Yuta Ushijima <register@yuta-u.com>
2020-02-06 02:25:57 +0900

    fix: Comment out of fontawesome

--- app/javascript/packs/app.ts
+++ app/javascript/packs/app.ts
@@ -4,7 +4,7 @@ import Router from "./router/router";
 import Header from "./container/Header.vue";
 import Vuetify from "vuetify";
 import "highlight.js/styles/monokai.css";
-import "@fortawesome/fontawesome-free/css/all.css"
+//import "@fortawesome/fontawesome-free/css/all.css"
 
 Vue.use(Vuetify);
 Vue.use(Vuetify, {
